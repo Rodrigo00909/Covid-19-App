c7a652e1b594e70654c25b2ce7f9db401d032c27
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-04-15 01:51:31 -0300

    jsx = mapeo of countries

--- src/FetchData.js
+++ src/FetchData.js
@@ -17,7 +17,26 @@ const FetchData = () => {
 
     return (
         <div>
-            {countries.length} countries
+            <section>
+                {countries.map(country => {
+                    const {ID, Country, CountryCode, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date} = country
+
+                    return (
+                        <article key={ID}>
+                            <h2>{Country}, {CountryCode}</h2>
+                            <ul>
+                                <li>{NewConfirmed}</li>
+                                <li>{TotalConfirmed}</li>
+                                <li>{NewDeaths}</li>
+                                <li>{TotalDeaths}</li>
+                                <li>{NewRecovered}</li>
+                                <li>{TotalRecovered}</li>
+                                <li>{Date}</li>
+                            </ul>
+                        </article>
+                    )
+                })}
+            </section>
         </div>
     )
 }
