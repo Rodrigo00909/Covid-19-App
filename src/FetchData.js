9e2bc4636d4cb9c38f788c347ba7be261d5ef2e9
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-04-15 18:31:27 -0300

    fetch data info + loading

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+
+import './FetchData.css';
+
 const url = `https://api.covid19api.com/summary`;
 
 const FetchData = () => {
     const [countries, setCountries] = useState([]);
+    const [isLoading, setIsLoagind] = useState(true);
 
     useEffect(() => {
         const fetchCountryData = async () => {
             const response = await axios.get(url);
             setCountries(response.data.Countries);
-            console.log(response.data.Countries);
+            setIsLoagind(false)
         }
 
         fetchCountryData()
@@ -17,26 +21,29 @@ const FetchData = () => {
 
     return (
         <div>
-            <section>
-                {countries.map(country => {
-                    const {ID, Country, CountryCode, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date} = country
+            {isLoading ? (<h1 className="Loading">Loading...</h1>) : (
+                <section>
+                    {countries.map(country => {
+                        const {ID, Country, CountryCode, NewConfirmed, TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date} = country;
 
-                    return (
-                        <article key={ID}>
-                            <h2>{Country}, {CountryCode}</h2>
-                            <ul>
-                                <li>{NewConfirmed}</li>
-                                <li>{TotalConfirmed}</li>
-                                <li>{NewDeaths}</li>
-                                <li>{TotalDeaths}</li>
-                                <li>{NewRecovered}</li>
-                                <li>{TotalRecovered}</li>
-                                <li>{Date}</li>
-                            </ul>
-                        </article>
-                    )
-                })}
-            </section>
+                        return (
+                            <article key={ID}>
+                                <h2>{Country}, {CountryCode}</h2>
+                                <ul>
+                                    <li>Ultimos casos confirmados: {NewConfirmed}</li>
+                                    <li>Total de casos confirmados: {TotalConfirmed}</li>
+                                    <li>Ultimas muertes: {NewDeaths}</li>
+                                    <li>Total de casos de muerte: {TotalDeaths}</li>
+                                    <li>Nuevos recuperados: {NewRecovered}</li>
+                                    <li>Total de recuperados: {TotalRecovered}</li>
+                                    <li>Fecha actual: {Date}</li>
+                                </ul>
+                            </article>
+                        )
+                    })}
+                </section>
+            
+            )}
         </div>
     )
 }
