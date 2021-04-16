7f6b0b1135dabff8956bcafcd65aa61c5c6bb77b
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-04-16 19:44:18 -0300

    toggle theme button test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, {useState, useEffect} from 'react';
 import FetchData from '../src/components/FetchData/FetchData';
 import Header from '../src/components/Header/Header';
 
 function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if(theme === 'light-theme'){
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme
+  },[theme]);
+
   return (
     <div>
       <Header />
+      <button onClick={toggleTheme}>THEME</button>
       <FetchData />
     </div>
   );
